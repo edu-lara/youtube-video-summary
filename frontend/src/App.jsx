de325486b30bc6975623a1754caaae5d739c18d7
Eduardo Lara <144066755+edu-lara@users.noreply.github.com>
2026-07-31 10:44:06 -0300

    fix: url variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-const API_URL = 'const API_URL = import.meta.env.VITE_API_URL;'
+const API_URL = import.meta.env.VITE_API_URL;
 
 const translations = {
   en: {
